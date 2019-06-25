e039c8745bde6945a348c65b9e6ffdcd56f4b7b6
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-06-25 15:00:44 -0700

    did some cleaning

--- jread.py
+++ jread.py
@@ -1,3 +1,10 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# jread.py  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Created by: Jordan Bonecutter for HPCForge  # # # # # # # # # # # # # #
+# 25 June 2019  # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# imports
 import json
 import re
 import PIL
@@ -8,30 +15,27 @@ from PIL import ImageFont as IMF
 PARSER = None
 
 # my regex string:
-# ^ = look for string start
-# (?: .*?\.)*
-#   - the parenthesis mean to create a new regex group
-#   - the ?: means that the regex compiler doesn't need 
-#     to save the result of this group 
-#     (we don't care about www. or ads.qs. or whatever)
-#   - the .*?\. means to look for the shortest string of 
-#     characters ending in a period
-#   - the * after the close paranthesis means to find the 
-#     most string of characters followed by periods
-#   - for https://google.com this would isolate https://
-# ([A-Za-z\d]+\.[A-Za-z\.]+)/
-#   - once again, the paranethesis means to find a group
-#   - [A-Za-z\d]+\. means to find a string of atleast length
-#     consisting of only characters in A-Z, a-z, or 0-9 ending
-#     with a period (for https://google.com/ this would isolate
-#     google)
-#   - Similarly, [A-Za-z\.]+/ means to find a string of atleast
-#     length one consisting of A-Z, a-z, and . ending in a / 
-#     (for https://google.com/ this would isolate com/)
-# .*$
-#   - this means to find any string of any length until you 
-#     hit the null character
-#magic_string = r"^(?:.*?\.)*([A-Za-z\d]+\.[A-Za-z\.]+)/.*$"
+# we want to find the "second level domain". to do so
+# we will read up until the top level domain and remeber
+# it and what came before it
+#
+# from the outside we have:
+# ()\/
+# we see a capture group followed by a literal / charcter.
+# since we want to capture up until the first /, we should 
+# capture what is immediately before it. Inside the capture 
+# group we have:
+# \w+\.\w{2,6}
+# remember, we want to capture the second level domain so 
+# we will read up until the last dot that is followed by a 
+# forward slash (\w{2,6} means any word character string in 
+# length between 2 and 6. for www.google.com/ this would isolate
+# the "com" part)
+# It is surrounded in paranthesis to capture it. if we were to
+# run it on the following url:
+# https://doubleclickads.net/4fxj/l
+# we would get:
+# doubleclickads.net
 magic_string = r"(\w+\.\w{2,6})\/"
 
 # use the json library to build a 
@@ -45,16 +49,32 @@ def get_json(fi):
 # domain name
 def get_url(url):
     global PARSER
+    # presumably we are going to call
+    # this function many times so compiling
+    # the regex will be faster
     if PARSER == None:
         PARSER = re.compile(magic_string)
 
+    # trim the string so the 
+    # regex takes faster
     if len(url) > 40:
         url = url[:40]
+
+    # catch the caller passing NULL
     if url == None:
         return "nil"
+
+    # if we didn't find a url, for example:
+    # about:none
+    # appears many times, we should return a
+    # string letting the caller know that we
+    # didn't get a match
     res = PARSER.search(url)
     if res == None:
         return "nil"
+
+    # we should return the first 
+    # regex capture group
     return res.group(1)
 
 def draw_tree(tree):
@@ -62,18 +82,27 @@ def draw_tree(tree):
     levels = [0]
     levels,width = get_levels(tree, 0, levels, 0)
 
-    # from the tree topology, we can find the size our 
-    # image needs to be
+    # eventually i should make these fit the tree
+    # size but that sounds like a lot of time
     radius = 200
     bufx   = 100
     bufy   = 400
+
+    # from the tree topology, we can find the size our 
+    # image needs to be
     size   = (2*(radius+bufx)*width, 2*(radius+bufy)*(len(levels)-1))
+
+    # create the bitmap to draw on
     bmp    = IMG.new("RGB", size, (255, 255, 255))
     draw   = IMD.Draw(bmp)
+
+    # params for the drawing function
     drawn  = [0]*len(levels)
     fnt    = IMF.truetype("Menlo-Regular.ttf", 50)
     draw_tree_sub(tree, 0, levels, draw, drawn, size, fnt, (-1,-1), radius, bufx, bufy)
     del draw
+
+    # save it, of course
     bmp.save("testtree3.png", "PNG")
 
 def draw_tree_sub(tree, curr, levels, draw, drawn, size, fnt, par_pos, rad, bufx, bufy):
@@ -122,10 +151,18 @@ def draw_tree_sub(tree, curr, levels, draw, drawn, size, fnt, par_pos, rad, bufx
 
 # get the topology of our tree
 def get_levels(tree, curr, levels, maxw):
+    # if we traversed to a new level, we
+    # need to add more space to the level 
+    # list
     if curr >= len(levels):
         levels.append(0)
+
+    # iterate through the children
     for item in tree.keys():
+        # increment the number of childen 
+        # at the current level of the tree 
         levels[curr] += 1
+        # also find the maxwidth of the tree
         if levels[curr] > maxw:
             maxw = levels[curr]
         t, maxw = get_levels(tree[item], curr+1, levels, maxw)
@@ -145,6 +182,9 @@ def tree_recurse(tree, head, parent_url):
     # get head url
     head_url = get_url(head['data'])
 
+    # if the redirect is to the same site or 
+    # it is the root node, we don't count this
+    # as a new level in the tree
     if head_url == "nil" or head_url == parent_url:
         if 'children' in head:
             for child in head['children']:
@@ -153,7 +193,6 @@ def tree_recurse(tree, head, parent_url):
 
     # try to put it as a new node on the tree
     if not head_url in tree:
-        #breakpoint()
         tree.update({head_url:{}})
 
     # if the node has no children, return
@@ -165,6 +204,3 @@ def tree_recurse(tree, head, parent_url):
         tree_recurse(tree[head_url], child, head_url)
 
     return tree
-
-def start():
-    return get_json('yh.json')
