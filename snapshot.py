8ee4184a8a229339124f0cc284ee7c875a3bea75
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-06-28 13:40:48 -0700

    Cleaned up snapshot

--- snapshot.py
+++ snapshot.py
@@ -5,13 +5,18 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 import os, io
-from contextlib import redirect_stdout
 
 def snap(url):
+    # Path on my machine to the ZBrowse file
     PATH = "../zbrowse/js/index.js "
+    # Use node.js to execute it
     EXEC = "node "
+    # Add the http prefix to the url
     PRFX = "http://www."
+    # Call the program redirecting stdout to a file
     os.system(EXEC + PATH + PRFX + url + " > tmp.snp")
+    # Read stdout
     t = open("tmp.snp", "r").read()
+    # Delete stdout tmp file
     os.system("rm -f tmp.snp")
     return t
