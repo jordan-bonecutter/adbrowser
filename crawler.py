9b2a93d6b54388ba39a44c48dfc29cca0b4c4059
Jordan Bonecutter <jordanbonecutter@gmail.com>
2019-07-02 15:32:57 -0700

    Fixed a lot. Fixed the Timeout handler so that it wouldn't catch the KeyboardInterrupt handler, completely changed the tree data structure because ZBrowse was having memory errors so I had to work around that. It is also more well suited for bfs which is nice for drawing the tree.

--- crawler.py
+++ crawler.py
@@ -18,6 +18,52 @@ SUBP         = None
 def snap(url, timeout):
     global REGEX, magic_string
 
+    # Clear browser cache
+    # I know, I know... this is really hideous
+    # This wouldn't work unless I cleared the files
+    # that start with a hex digit so this is the best
+    # got. TODO: make this better (?)
+    c_loc = "/Users/jordanbonecutter/Library/Caches/Google/Chrome\ Canary/Default/"
+    os.system("rm -rf " + c_loc + "Cache/0*") 
+    os.system("rm -rf " + c_loc + "Cache/1*") 
+    os.system("rm -rf " + c_loc + "Cache/2*") 
+    os.system("rm -rf " + c_loc + "Cache/3*") 
+    os.system("rm -rf " + c_loc + "Cache/4*") 
+    os.system("rm -rf " + c_loc + "Cache/5*") 
+    os.system("rm -rf " + c_loc + "Cache/6*") 
+    os.system("rm -rf " + c_loc + "Cache/7*") 
+    os.system("rm -rf " + c_loc + "Cache/8*") 
+    os.system("rm -rf " + c_loc + "Cache/9*") 
+    os.system("rm -rf " + c_loc + "Cache/a*") 
+    os.system("rm -rf " + c_loc + "Cache/b*") 
+    os.system("rm -rf " + c_loc + "Cache/c*") 
+    os.system("rm -rf " + c_loc + "Cache/d*") 
+    os.system("rm -rf " + c_loc + "Cache/e*") 
+    os.system("rm -rf " + c_loc + "Cache/f*") 
+    os.system("rm -rf " + c_loc + "Cache/index")
+    os.system("rm -rf " + c_loc + "Cache/index-dir/*")
+
+    os.system("rm -rf " + c_loc + "Code Cache/js/0*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/1*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/2*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/3*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/4*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/5*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/6*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/7*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/8*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/9*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/a*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/b*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/c*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/d*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/e*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/f*") 
+    os.system("rm -rf " + c_loc + "Code Cache/js/index")
+    os.system("rm -rf " + c_loc + "Code Cache/js/index-dir/*")
+
+    os.system("rm -rf " + c_loc + "Storage/ext/*")
+
     # Path on my machine to the ZBrowse file
     PATH = "../zbrowse/js/index.js"
     # Use node.js to execute it
@@ -27,13 +73,15 @@ def snap(url, timeout):
     # Redirect stdout to a file
     new_sout = open("sout.snp", "w")
     new_serr = open("serr.snp", "w")
+    extra    = "--max_old_space_size=4096"
     # Execute the node.js program
-    SUBP = subprocess.Popen(args=(EXEC, PATH, PRFX+url), stdout=new_sout, stderr=new_serr)
+    SUBP = subprocess.Popen(args=(EXEC, extra, PATH, PRFX+url), stdout=new_sout, stderr=new_serr)
     # We want to wait on a timeout so 
     # we need to try & except
     try:
         # Wait
         SUBP.wait(timeout)
+        SUBP.kill()
         # Close the files so they save
         new_sout.close()
         new_serr.close()
@@ -48,6 +96,7 @@ def snap(url, timeout):
             ec.close()
             # Set the tree to empty
             t = "{}"
+            print("JS ran out of memory")
         # Otherwise if the run was succesful
         else:
             # Reopen it 
@@ -61,7 +110,7 @@ def snap(url, timeout):
             # Remove it
             os.system("rm -r *.snp")
             SUBP = None
-    except:
+    except subprocess.TimeoutExpired:
         # If we timed out, kill the subproc
         SUBP.kill()
         SUBP = None
@@ -71,11 +120,12 @@ def snap(url, timeout):
         new_sout.close()
         # Remove temp file
         os.system("rm -r *.snp")
+        print("Timeout")
     return t
 
 def crawl(sites):
     # local variables
-    t0    = 1
+    t0    = 45
 
     start = 0
     end   = 0
@@ -92,8 +142,9 @@ def crawl(sites):
             # We will time the snap so that
             # we can adjust the timeout value
             # accordingly
+            t0    = sites[site][1]
             start = TIME.time()
-            s     = snap(site, t0)
+            s     = snap(site, t0+5)
             end   = TIME.time()
             tot   = end-start
             # If we timed out
@@ -106,7 +157,9 @@ def crawl(sites):
             # large values, it's better to not timeout
             # than to timeout)
             else:
-                t0 = (((t0*t0)+((tot+1)*(tot+1)))/(t0+tot))
+                t0 = (((t0*t0)+((tot)*(tot)))/(t0+tot))
+    
+            sites[site][1] = t0
 
             # If for whatever reason the snapshot
             # failed, continue looping
@@ -115,15 +168,17 @@ def crawl(sites):
 
             # Extract the tree
             t = JREAD.get_tree(s)
+            JREAD.draw_tree(t, "./sites/" + site + str(sites[site][0]) + ".png")
 
             # Save it to a file
-            f = open("sites/" + site + "[" + str(sites[site]) + "].json", "w")
+            print("Completed scraping " + site + " in " + str(tot) + " seconds")
+            f = open("sites/" + site + "[" + str(sites[site][0]) + "].json", "w")
             JSON.dump(t, f)
             f.close()
-            sites[site] += 1
+            sites[site][0] += 1
 
 def main():
-    SITES = {"yahoo.com": 0, "forbes.com": 0, "bbc.com": 0}
+    SITES = {"yahoo.com": [0,45], "forbes.com":[0,45], "bbc.com":[0,45], "cnn.com":[0,45]}
     crawl(SITES)
 
 # main function call
@@ -133,4 +188,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Interrupted")
         if SUBP != None:
+            os.system("rm -rf *.snp")
             os.kill(SUBP.pid, signal.SIGTERM)
--- jread.py
+++ jread.py
@@ -12,14 +12,15 @@ from PIL import Image as IMG
 from PIL import ImageDraw as IMD
 from PIL import ImageFont as IMF
 
-PARSER = None
+URL_PARSER  = None
+TREE_PARSER = None
 
-# my regex string:
+# url regex string:
 # we want to find the "second level domain". to do so
 # we will read up until the top level domain and remeber
 # it and what came before it
 #
-# from the outside we have:
+# from the outside we have:\
 # ()\/
 # we see a capture group followed by a literal / charcter.
 # since we want to capture up until the first /, we should 
@@ -36,24 +37,29 @@ PARSER = None
 # https://doubleclickads.net/4fxj/l
 # we would get:
 # doubleclickads.net
-magic_string = r"(\w+\.\w{2,6})\/"
+url_string  = r"(\w+\.\w{2,6})\/"
 
-# use the json library to build a 
-# dictionary out of the json
-def get_json(fi):
-    contents = open(fi, 'r').read()
-    return json.loads(contents)
+# tree regex string:
+# since the output from Zbrowse has been configured to be:
+# c:<child_url>
+# p:<parent_url>
+# ...
+# 
+# we will make a regex who captures the child url & the parent url
+# it will look for "c:" and then pature whatever comes before the \n
+# it will then look for "p:" and pature anything till newline or EOF
+tree_string = r"c:(.+)\np:(.+)(?:\n|$)"
 
 # use the magic string and regex to 
 # isolate the important part of a 
 # domain name
 def get_url(url):
-    global PARSER
+    global URL_PARSER
     # presumably we are going to call
     # this function many times so compiling
     # the regex will be faster
-    if PARSER == None:
-        PARSER = re.compile(magic_string)
+    if URL_PARSER == None:
+        URL_PARSER = re.compile(url_string)
 
     # trim the string so the 
     # regex takes faster
@@ -69,7 +75,7 @@ def get_url(url):
     # appears many times, we should return a
     # string letting the caller know that we
     # didn't get a match
-    res = PARSER.search(url)
+    res = URL_PARSER.search(url)
     if res == None:
         return "nil"
 
@@ -77,129 +83,165 @@ def get_url(url):
     # regex capture group
     return res.group(1)
 
-def draw_tree(tree):
-    # get the tree topology
-    levels = [0]
-    levels,width = get_levels(tree, 0, levels, 0)
+def draw_tree(tree, outname):
+    # get the layout of the tree
+    structure = []
+    for i in range(0, len(tree)):
+        structure.append({})
+    i = 0
+    for level in tree:
+        j = 0
+        curr = structure[i]
+        for key in level.keys():
+            curr.update({key:j})
+            j += 1
+        i += 1
 
-    # eventually i should make these fit the tree
-    # size but that sounds like a lot of time
-    radius = 200
-    bufx   = 100
-    bufy   = 400
+    # get the height and max width
+    # of the tree
+    h = len(structure)
+    w = 0
+    for level in structure:
+        if len(level) > w:
+            w = len(level)
 
-    # from the tree topology, we can find the size our 
-    # image needs to be
-    size   = (2*(radius+bufx)*width, 2*(radius+bufy)*(len(levels)-1))
+    # picture parameters
+    rad  = 200
+    bufx = 100
+    bufy = 300
+    thck = 10
 
-    # create the bitmap to draw on
-    bmp    = IMG.new("RGB", size, (255, 255, 255))
-    draw   = IMD.Draw(bmp)
+    # get the image size
+    size = (bufx + w*(bufx+(2*rad)), bufy + h*(bufy+(2*rad)))
 
-    # params for the drawing function
-    drawn  = [0]*len(levels)
-    fnt    = IMF.truetype("Menlo-Regular.ttf", 50)
-    draw_tree_sub(tree, 0, levels, draw, drawn, size, fnt, (-1,-1), radius, bufx, bufy)
-    del draw
+    # create a new image
+    img  = IMG.new ("RGB", size, (255, 255, 255))
+    draw = IMD.Draw(img)
+    fnt  = IMF.truetype("Menlo-Regular.ttf", 50)
 
-    # save it, of course
-    bmp.save("testtree3.png", "PNG")
+    # draw!
+    plev = -1
+    c0xp = 0
+    c0yp = 0
+    slen = len(structure)
+    # iterate through the levels of the tree
+    for level in range (0, len(structure)):
+        # get the width of the current level
+        llen = len(structure[level])
 
-def draw_tree_sub(tree, curr, levels, draw, drawn, size, fnt, par_pos, rad, bufx, bufy):
-    # iterate through subtree
-    for key in tree.keys():
-        # we want to draw the nodes so that each level is centered
-        # so what we should do is find the center of the leftmost
-        # circle (c0) and then add an offset for the current circle
-        #
-        # the center of the leftmost circle will be the center of the
-        # page - half the range of all the circles (if you draw this
-        # out you will see it). so i do this math for both the x & y 
-        # coords and draw each circle accordingly
+        # find the x center coordinate
         ctr = size[0]/2
-        rng = (2*(bufx+rad))*levels[curr] - (2*bufx)
-        c0  = (ctr - (rng/2)) + rad
-        cx  = (2*(rad+bufx))*drawn[curr] + c0
+        rng = llen*(2*rad) + (llen-1)*bufx
+        c0x = ctr - (rng/2) + rad
 
+        # find the y center coordinate
         ctr = size[1]/2
-        rng = (2*(bufy+rad))*(len(levels)-1) - (2*bufy)
-        c0  = (ctr - (rng/2)) + rad
-        cy  = (2*(rad+bufy))*curr + c0
+        rng = slen*(2*rad) + (slen-1)*bufy
+        c0y = ctr - (rng/2) + rad
+        cy  = c0y + level*(2*rad + bufy)
+        for item in structure[level].keys():
+            # since we are iterating "horizontally", we
+            # need only update the x position
+            cx  = c0x + structure[level][item]*(2*rad + bufx)
+            
+            # draw ecntered text
+            textsize = draw.textsize(item, font=fnt)
 
-        # actually draw the circle
-        draw.ellipse([cx-rad, cy-rad, cx+rad, cy+rad], (50, 0, 255), (50, 0, 255))
-        # draw the text cenetered in the circle
-        w,h = draw.textsize(key, font=fnt)
-        draw.text((cx-w/2, cy-h/2), key, font=fnt, fill=(0, 255, 100))
-        # draw a line connecting it to its parent
-        # we want the lines to have some separation so that we can tell 
-        # who their parent is. so each connection will have 3 segements, the
-        # first segment moves purely up, the second segment moves purely horizontal
-        # and the third segment moves purely up again. the first segment will move up 
-        # different lengths depending on the node, so that's why there's some extra math.
-        # basically, you start at angle pi/2 for the child circle (cx, cy-rad) then you move
-        # up towards the y coordinate of the next circle cy-rad + dy (dy being that long
-        # next part). then you move from that coord to 3pi/2 on the parent (p[0], p[1]+rad)
-        if par_pos[0] > 0:
-            draw.line([(cx, cy-rad), (cx, cy-rad + (drawn[curr]+1)*((par_pos[1]+rad-cy+rad))/(levels[curr]+2)), (par_pos[0], cy-rad + (drawn[curr]+1)*((par_pos[1]+rad-cy+rad))/(levels[curr]+2)), (par_pos[0], par_pos[1]+rad)], (0, 255, 100), 10)
+            # draw the circle + text
+            draw.ellipse([cx-rad, cy-rad, cx+rad, cy+rad], (50, 0, 255), (50, 0, 255))
+            draw.text([cx-textsize[0]/2, cy-textsize[1]/2], item, font=fnt, fill=(0,0,0))
+            
+            # if we aren't at the top level then
+            # draw a line connecting it to its parent(s)
+            if plev != -1:
+                # iterate through parents
+                for parent in tree[level][item].keys():
+                    # get the index of the parent
+                    pindex = structure[level-1][parent]
+                    cpx    = c0xp + (pindex)*(2*rad + bufx)
+                    cpy    = c0yp
+                
+                    # get the width of the parent layer
+                    plsize = len(structure[level-1])
+                    # calculate on offset so that lines dont intersect
+                    offset = (pindex+1)*bufy/(plsize+2)
+                    # draw 3 lines to connect the parent and child
+                    draw.line([(cx, cy-rad), (cx, cy-rad-offset-thck/2)], (0,0,0),thck)
+                    draw.line([(cx, cy-rad-offset), (cpx, cy-rad-offset)], (0,0,0), thck)
+                    draw.line([(cpx, cy-rad-offset+thck/2), (cpx, cpy+rad)], (0,0,0), thck)
 
-        # increment drawn
-        drawn[curr] += 1
+        # remember the parent level and center
+        # of the circle at the beginning of the parent level
+        plev = level
+        c0xp = c0x
+        c0yp = cy
 
-        #recurse!
-        draw_tree_sub(tree[key], curr+1, levels, draw, drawn, size, fnt, (cx, cy), rad, bufx, bufy)
+    # delete the drawing object
+    del draw
+    # save the image
+    img.save(outname, "PNG")
 
-# get the topology of our tree
-def get_levels(tree, curr, levels, maxw):
-    # if we traversed to a new level, we
-    # need to add more space to the level 
-    # list
-    if curr >= len(levels):
-        levels.append(0)
-
-    # iterate through the children
-    for item in tree.keys():
-        # increment the number of childen 
-        # at the current level of the tree 
-        levels[curr] += 1
-        # also find the maxwidth of the tree
-        if levels[curr] > maxw:
-            maxw = levels[curr]
-        t, maxw = get_levels(tree[item], curr+1, levels, maxw)
-    return levels, maxw
-
-# build the dependency tree
-def get_tree(js):
-    # start at the root node
-    js   = json.loads(js)
-    head = js['_root']
-    tree = {}
-    tree = tree_recurse(tree, head, "no")
-    return tree
-
-def tree_recurse(tree, head, parent_url):
-    # get head url
-    head_url = get_url(head['data'])
-
-    # if the redirect is to the same site or 
-    # it is the root node, we don't count this
-    # as a new level in the tree
-    if head_url == "nil" or head_url == parent_url:
-        if 'children' in head:
-            for child in head['children']:
-                tree_recurse(tree, child, parent_url)
-        return tree
-
-    # try to put it as a new node on the tree
-    if not head_url in tree:
-        tree.update({head_url:{}})
-
-    # if the node has no children, return
-    if not 'children' in head:
-        return tree
+def get_tree(s):
+    # save a regex for parsing the tree
+    global TREE_PARSER
+    if TREE_PARSER == None:
+        TREE_PARSER = re.compile(tree_string)
     
-    # iterate through children
-    for child in head['children']:
-        tree_recurse(tree[head_url], child, head_url)
+    # init an empty tree
+    tree    = [{}]
+    matches = TREE_PARSER.findall(s)
+    # iterate through the matches and 
+    # bild the tree
+    for match in matches:
+        c_url = get_url(match[0])
+        p_url = get_url(match[1])
+        # if the url is garbage or it is
+        # a self redirect which we don't
+        # really care about
+        if c_url == "nil" or p_url == "nil" or c_url == p_url:
+            continue
+        p_lyr = tree_bfs(tree, p_url)
+
+        # if the parent is not in the tree
+        if p_lyr == -1:
+            # add it to the root
+            tree[0].update({p_url:{"_root":1}})
+            if len(tree) == 1:
+                tree.append({})
+
+            # see if the child exists in 
+            # the next layer
+            if c_url in tree[1]:
+                if p_url in tree[1][c_url]:
+                    tree[1][c_url][p_url] += 1
+                else:
+                    tree[1][c_url].append(p_url)
+            else:
+                tree[1].update({c_url:{p_url:1}})
+
+        # if the parent is in the tree
+        else:
+            if len(tree) == p_lyr+1:
+                tree.append({})
+            
+            # see if the child exists in 
+            # the next layer
+            if c_url in tree[p_lyr+1]:
+                if p_url in tree[p_lyr+1][c_url]:
+                    tree[p_lyr+1][c_url][p_url] += 1
+                else:
+                    tree[p_lyr+1][c_url].append(p_url)
+            else:
+                tree[p_lyr+1].update({c_url:{p_url:1}})
 
     return tree
+
+def tree_bfs(tree, item):
+    # search "horizontally"
+    for i in range(0, len(tree)):
+        if item in tree[i]:
+            return i
+    # item is not in the tree
+    return -1
+
+# eof #
